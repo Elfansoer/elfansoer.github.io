bbd9dcfb7520de410b8890eb85635b1f483224ea
unknown <elfansoer@gmail.com>
2024-08-19 05:14:47 +0900

    try

--- docs/index.js
+++ docs/index.js
@@ -9,8 +9,30 @@ function newPage(id, root) {
     div.innerText = "Some text here";
     root.appendChild(div);
 }
+function getJobName() {
+    const myJobs = [
+        "Programmer",
+        "Gamer",
+        "Nature Viewer",
+        "Culture Appreciator"
+    ];
+    const randInt = Math.floor(Math.random() * myJobs.length);
+    return myJobs[randInt];
+}
 window.onload = () => {
     console.log("Hello, world!");
-    newPage("test", document.body);
+    // newPage("test",document.body);
+    const main = document.getElementById("home");
+    if (main) {
+        const randomFun = () => {
+            const test = main.querySelector('#custom-job');
+            if (test) {
+                test.innerHTML = getJobName();
+            }
+        };
+        main.onclick = randomFun;
+        main.onmouseenter = randomFun;
+        main.onmouseleave = randomFun;
+    }
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,34 @@ function newPage(id: string, root: HTMLElement) {
     root.appendChild(div);
 }
 
+function getJobName() {
+    const myJobs = [
+        "Programmer",
+        "Gamer",
+        "Nature Viewer",
+        "Culture Appreciator"
+    ];
+
+    const randInt = Math.floor( Math.random() * myJobs.length );
+    return myJobs[randInt];
+}
+
+
 window.onload = ()=>{
     console.log("Hello, world!");
-    newPage("test",document.body);
+    // newPage("test",document.body);
+
+    const main = document.getElementById("home");
+    if (main) {
+        const randomFun = ()=>{
+            const test = main.querySelector('#custom-job');
+            if (test) {
+                test.innerHTML = getJobName();
+            }
+        }
+
+        main.onclick = randomFun;
+        main.onmouseenter = randomFun;
+        main.onmouseleave = randomFun;
+    }    
 }
